619679c88f3effcc0ae537598df0ab85d4a3d15c
DingGuodong <uberurey_ups@163.com>
2016-05-21 15:35:18 +0800

    add new files

---
+++ functions/description/descriptiongems.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Progress: 0% (Rate: 106k/s, Estimated time remaining: 8:31:25)
+# Estimated time remaining
+# ==> default: Adding box
---
+++ functions/file/download_file.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#nohup curl -q --fail --location --max-redirs 10 --user-agent 'Vagrant/1.8.1 (+https://www.vagrantup.com; ruby2.2.3)' --continue-at - --output /root/box3c0998296d01dea7d3da811797eac21584a8b71a http://files.edx.org/vagrant-images/20151221-dogwood-fullstack-rc2.box &
+#curl -q --fail --location --max-redirs 10 --user-agent 'Vagrant/1.8.1 (+https://www.vagrantup.com; ruby2.2.3)' --continue-at - --output /tmp/curl_path_to_file http://files.edx.org/vagrant-images/20151221-dogwood-fullstack-rc2.box
+#wget -c <url>
