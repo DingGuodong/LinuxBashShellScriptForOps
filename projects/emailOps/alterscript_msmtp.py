58a6ea71dd0e1b247a2266f469c64891e035ee1d
DingGuodong <uberurey_ups@163.com>
2016-06-06 11:16:22 +0800

    add new files

--- projects/emailOps/alterscript_msmtp.py
+++ projects/emailOps/alterscript_msmtp.py
@@ -1,5 +1,16 @@
 import smtplib
 import string
+import sys
+
+
+def usage():
+    print("""
+    Function: send email to somebody using smtp protocol
+    Usage: %s <mailto> <subject> <message body>
+    Example: python %s "dinggd@huntor.cn" "Test email from Python" "Python rules them all!"
+""") % (__file__, sys.argv[0])
+    sys.exit(0)
+
 
 EMAIL_HOST = "smtp.exmail.qq.com"
 EMAIL_PORT = 25
@@ -11,6 +22,19 @@ CRLF = "\r\n"  # for Windows user
 EMAIL_TO = "dinggd@huntor.cn"  # user defined variable
 SUBJECT = "Test email from Python"  # user defined variable
 text = "Python rules them all!"  # user defined variable
+
+argc = len(sys.argv)
+if not (argc == 1 or argc == 4):
+    print("Error: incorrect number of arguments or unrecognized option")
+    usage()
+if argc == 1:
+    pass
+else:
+    if sys.argv[1] is not None and sys.argv[2] is not None and sys.argv[3] is not None:
+        EMAIL_TO = sys.argv[1]
+        SUBJECT = sys.argv[2]
+        text = sys.argv[3]
+
 BODY = string.join((
     "From: %s" % DEFAULT_FROM_EMAIL,
     "To: %s" % EMAIL_TO,
