c54c494098973b96250b40cd0f28c18bb2076f3b
DingGuodong <uberurey_ups@163.com>
2016-06-25 17:54:21 +0800

    add new files

--- projects/autoOps/pythonSelf/fabfile.py
+++ projects/autoOps/pythonSelf/fabfile.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 # encoding: utf-8
 # -*- coding: utf8 -*-
+# Refer: http://docs.fabfile.org/en/1.6/tutorial.html#conclusion
 # Refer: https://github.com/dlapiduz/fabistrano/blob/master/fabistrano/deploy.py
 # Refer: https://gist.github.com/mtigas/719452
 # fab -f .\projects\autoOps\pythonSelf\fabfile.py dotask -i c:\Users\Guodong\.ssh\exportedkey201310171355
 
 import os
 import datetime
+import re
+import sys
 
 try:
     from fabric.api import *
@@ -18,6 +21,8 @@ except ImportError:
         exit(1)
 finally:
     from fabric.api import *
+    from fabric.main import main
+    from fabric.colors import *
 
 try:
     import pytz
@@ -35,6 +40,7 @@ env.roledefs = {
     'productionEnvironment': ['root@10.6.28.28:22', ],
 }
 
+env.git_address = ''
 env.basedir = '/root/www'
 env.capistrano_ds_lock = env.basedir + '/.capistrano_ds_lock'
 
@@ -60,9 +66,9 @@ def check_network_connectivity():
     except Exception:
         result_code = 1
     if result_code is not None:
-        print "Error   => connect to Internet failed!"
+        print red("Error   => connect to Internet failed!")
     else:
-        print "Success => connect to Internet successfully!"
+        print green("Success => connect to Internet successfully!")
 
 
 def check_name_resolve():
@@ -74,30 +80,52 @@ def check_name_resolve():
     except Exception:
         result_code = 1
     if result_code is not None:
-        print "Error   => name resolve to Internet failed!"
+        print red("Error   => name resolve to Internet failed!")
     else:
-        print "Success => name resolve to Internet successfully!"
+        print green("Success => name resolve to Internet successfully!")
 
 
 def set_capistrano_directory_structure_over_fabric():
+    print blue("setting capistrano directory structure ...")
     capistrano_release = env.basedir + '/release'
     capistrano_repository = env.basedir + '/repository'
     capistrano_share = env.basedir + '/share'
     capistrano_backup = env.basedir + '/backup'
-    if os.path.exists(env.basedir):
+    if os.path.exists(env.capistrano_ds_lock):
         pass
     else:
         if not os.path.exists(capistrano_release):
-            os.mkdir(capistrano_release)
+            os.makedirs(capistrano_release)
         if not os.path.exists(capistrano_repository):
-            os.mkdir(capistrano_repository)
+            os.makedirs(capistrano_repository)
         if not os.path.exists(capistrano_share):
-            os.mkdir(capistrano_share)
+            os.makedirs(capistrano_share)
         if not os.path.exists(capistrano_backup):
-            os.mkdir(capistrano_backup)
-        with open(env.capistrano_ds_lock, 'w') as f:
-            f.write(str(datetime.datetime.now()))
+            os.makedirs(capistrano_backup)
 
-        if os.path.exists("/etc/timezone"):
-            tz = file("/etc/timezone").read().strip()
-            print pytz.timezone(tz)
+        with open(env.capistrano_ds_lock, 'w') as f:
+            if os.path.exists("/etc/timezone"):
+                tz = file("/etc/timezone").read().strip()
+                if tz == 'Asia/Chongqing' or tz == 'Asia/Shanghai':
+                    content = datetime.datetime.now(tz=pytz.timezone(tz))
+                else:
+                    content = datetime.datetime.now(tz=pytz.timezone('Asia/Shanghai'))
+            else:
+                content = datetime.datetime.now()
+            f.write(str(content))
+    print green("setting capistrano directory structure successfully!")
+
+
+def set_capistrano_directory_structure_local():
+    local(set_capistrano_directory_structure_over_fabric())
+
+
+def git_clone():
+    code_dir = env.basedir + '/repository'
+    local()
+
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    print "Please use 'fab -f %s'" % sys.argv[0:]
+    sys.exit(1)
