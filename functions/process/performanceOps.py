85668ca026c902248bff98f059d26a4ce3c35024
DingGuodong <uberurey_ups@163.com>
2016-09-22 15:53:30 +0800

    Python以表格的形式打印占用内存Top10的程序列表

---
+++ functions/process/performanceOps.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File:               LinuxBashShellScriptForOps:performanceOps.py
+User:               Guodong
+Create Date:        2016/9/21
+Create Time:        18:11
+ """
+import psutil
+import prettytable
+
+ps_result = list()
+
+for proc in psutil.process_iter():
+    ps_result.append({'name': proc.name(), 'pid': proc.pid, 'cpu_percent': proc.cpu_percent(),
+                      'memory_percent': proc.memory_percent()})
+
+table = prettytable.PrettyTable()
+table.field_names = ["No.", "Name", "Memory percent"]
+for i, item in enumerate(sorted(ps_result, key=lambda x: x['memory_percent'], reverse=True)):
+    table.add_row([i + 1, item['name'], format(item['memory_percent'] / 100, '.2%')])
+    if i >= 9:
+        break
+print table
