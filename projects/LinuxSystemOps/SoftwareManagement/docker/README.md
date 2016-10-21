bd0a6a915974c8b34bd022a0ef5cdd76b22fff5a
DingGuodong <uberurey_ups@163.com>
2016-10-21 18:00:03 +0800

    stage update: add a simple way to get Container ID.

--- projects/LinuxSystemOps/SoftwareManagement/docker/README.md
+++ projects/LinuxSystemOps/SoftwareManagement/docker/README.md
@@ -1,5 +1,7 @@
 # About getDockerContainerInfoWithPidNumber.py
 
+### What problem to solve?
+
 Usually, we can NOT find a process comes from which docker container using with 'ps', etc commands on host.
 
 通常我们不能在主机上通过“ps”等命令判断一个docker进程来自哪一个容器。
@@ -8,4 +10,12 @@ With this file, you can input a pid number to it, and you will get a useful low-
 'docker inspect \<id\>' to
 help you find out which container does it come from.
 
-借助这个文件，给它输入一个pid数字，它就能调用 'docker inspect \<id\>'命令打印出详细的信息，帮助你判断这个进程来自哪一个容器。
+借助这个文件，给它输入一个pid数字，它就能调用 'docker inspect \<id\>'命令打印出详细的信息，
+帮助你判断这个进程来自哪一个容器。
+
+### Requirement:
+
+Docker Server Version >= 1.11, you can use 'docker info' to find docker server version out.
+
+要求：
+Docker Server Version >= 1.11，可以使用'docker info '判断docker server的版本。
--- projects/LinuxSystemOps/SoftwareManagement/docker/getDockerContainerInfoWithPidNumber.py
+++ projects/LinuxSystemOps/SoftwareManagement/docker/getDockerContainerInfoWithPidNumber.py
@@ -18,6 +18,67 @@ class dockerContainerUtils(object):
         processInformationPseudoFileSystem = "/proc"
         self.proc = processInformationPseudoFileSystem
         self.debugEnabled = True
+        self.pid = 0
+        self.pid = self.getPidInput()
+        self.checkDockerServerVersion()
+
+    def getContainerLongIdUsingDockerInspectAllContainerID(self):
+        import subprocess
+        result = subprocess.Popen(r"docker inspect -f '{{.State.Pid}} {{.Id}}' $(docker ps -a -q) | grep %s" % self.pid,
+                                  shell=True, stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
+        idList = result.stdout.read().strip().split(' ')
+        if len(idList) != 0:
+            return idList
+        else:
+            print "Container ID is NOT found for pid %s, maybe this is NOT a container pid! Now exit!" % self.pid
+            pass  # sys.exit(1)
+
+    @staticmethod
+    def checkDockerServerVersion():
+        versionSupported = "1.11.0"
+        import subprocess
+        result = subprocess.Popen("docker info", shell=True, stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
+
+        # TODO(Guodong Ding) returncode returns bad 'NoneType'.
+        # if int(result.returncode) == 0:
+        #     pass
+        # else:
+        #     raise RuntimeError(
+        #         '\'docker info\' run failed! This always means docker service is down or broken. Aborted!')
+
+        import re
+        pattern = re.compile(r'Server Version: ([0-9\.]+)')
+        match = pattern.findall(result.stdout.read().strip())
+        if match:
+            version = match[0]
+        else:
+            raise RuntimeError('Unknown error. Aborted!')
+        if version > versionSupported:
+            print "Docker Server Version is: %s. Congratulations! This version is supported!" % version
+            return True
+        else:
+            print "Docker Server Version is: %s. Unfortunately! This version is NOT supported!" % version
+            return False
+
+    def getPidInput(self):
+        self.pid = int(raw_input('Please input pid number.'))
+        if self.isLegalPid() and self.isExistedPid():
+            return self.pid
+
+    def isLegalPid(self):
+        if str(self.pid).isdigit() and (int(self.pid) >= 0 or int(self.pid) <= 32768):
+            return True
+        else:
+            raise RuntimeError('Pid input should be an integer, and should be 0~32768. Aborted!')
+
+    def isExistedPid(self):
+        if os.path.exists(os.path.join(self.proc, str(self.pid), 'cmdline')) and os.path.exists(
+                os.path.join(self.proc, str(self.pid), 'status')):
+            return True
+        else:
+            raise RuntimeError('pid is NOT exist! Aborted! ')
 
     def debug(self, objects):
         if not self.debugEnabled:
@@ -56,22 +117,22 @@ class dockerContainerUtils(object):
         if pid in self.getPids():
             pidDir = os.path.join(self.proc, str(pid))
         else:
-            print 'pid is not exist!'
+            print 'pid is not exist! Aborted!'
             sys.exit(1)
 
         if os.path.exists(pidDir):
             pass
         else:
-            print 'pid is not exist!'
+            print 'pid is not exist! Aborted!'
             sys.exit(1)
 
         try:
             with open(os.path.join(pidDir, "cmdline"), 'r') as f:
                 cmdline = f.read().strip().replace('\x00', ' ').strip()
                 if cmdline is None or cmdline == '':
-                    raise RuntimeError('cmdline is None or \'\'')
+                    raise RuntimeError('cmdline is None or \'\', this always means pid is NOT exist! Aborted! ')
         except IOError:
-            print 'pid is not exist!'
+            print 'pid is not exist! Aborted!'
             sys.exit(1)
         return str(cmdline)
 
@@ -79,7 +140,7 @@ class dockerContainerUtils(object):
         status = self.getStatus(pid)
         # TODO(Guodong Ding) self.getStatus(pid) won't quit right
         if len(status) == 0:
-            raise KeyError("getStatus(Type: OrderedDict) returns None, this always means pid is NOT exist!")
+            raise KeyError("getStatus(Type: OrderedDict) returns None, this always means pid is NOT exist! Aborted!")
         return int(status['PPid'])
 
     def getPPidCmdline(self, pid):
@@ -133,15 +194,79 @@ class dockerContainerUtils(object):
         match = re.findall(r'(?<=docker-containerd-shim )\S+', string)
         if len(match) != 0:
             return match
+        elif len(self.getContainerShortIDWithPort(pid)) != 0:
+            return self.getContainerShortIDWithPort(pid)
         else:
-            return None
+            return []
+
+    @staticmethod
+    def getFDSocketInodeNumber(pid):
+        inodeList = list()
+        try:
+            for f in os.listdir("/proc/%d/fd" % pid):
+                socket = os.readlink("/proc/%d/fd/%s" % (pid, f))
+                if 'socket' in socket:
+                    import re
+                    pattern = re.compile(r'\[(\d+)\]')
+                    match = pattern.search(socket)
+                    if match:
+                        inodeList.append(match.groups()[0])
+        except OSError:
+            raise RuntimeError("Can NOT open directory \'/proc/%d/fd\'. Aborted!" % pid)
+
+        return inodeList
+
+    def getPortNumberWithInodeNumber(self, pid):
+        inodeList = self.getFDSocketInodeNumber(pid)
+        portList = list()
+        try:
+            with open('/proc/net/tcp', 'r') as f:
+                tcp = f.read()
+        except IOError:
+            raise RuntimeError("Can NOT open file \'/proc/net/tcp\'")
+
+        try:
+            with open('/proc/net/tcp6', 'r') as f:
+                tcp6 = f.read()
+        except IOError:
+            raise RuntimeError("Can NOT open file \'/proc/net/tcp6\'")
+
+        for inodeNumber in inodeList:
+            import re
+            pattern = re.compile(r'.*\d+:\s\d+:(\d+).*%s.*' % inodeNumber)
+            match = pattern.findall(tcp.strip())
+            if match:
+                portList.append(int(match[0], 16))
+
+            match6 = pattern.findall(tcp6)
+            if match6:
+                portList.append(int(match6[0], 16))
+
+        return list(set(portList))
+
+    def getContainerShortIDWithPort(self, pid):
+        # TODO(Guodong Ding) pid should use 'docker-proxy's' pid.
+        portList = self.getPortNumberWithInodeNumber(pid)
+        import subprocess
+        result = subprocess.Popen("docker ps ", shell=True, stdout=subprocess.PIPE,
+                                  stderr=subprocess.STDOUT)
+        if len(portList) != 0:
+            for port in portList:
+                import re
+                pattern = re.compile(r'([a-z0-9]+) .*%s.*' % port)
+                match = pattern.findall(result.stdout.read().strip())
+                if match:
+                    return match[0]
+        else:
+            return []
 
     def getContainerInfo(self, pid):
         containerId = self.getContainerId(pid)
-        if len(containerId):
+        if len(containerId) != 0:
             import subprocess
             result = subprocess.Popen("docker inspect %s" % containerId[0], shell=True, stdout=subprocess.PIPE,
                                       stderr=subprocess.STDOUT)
+            # also can get this from /run/runc/<id>/state.json or /run/containerd/<id>/state.json
             import json
             result_string = json.dumps(result.stdout.read())
             result_object = json.loads(result_string)
@@ -154,11 +279,18 @@ if __name__ == '__main__':
     pidNumber = int(raw_input('Please input pid number.'))
     p = dockerContainerUtils()
     print "Pid number is: %d" % pidNumber
+    print "Pid number ContainerId is %s" % p.getContainerLongIdUsingDockerInspectAllContainerID()
     print "Pid number cmdline is: %s" % p.getCmdline(pidNumber)
     print "Parent pid number is: %s" % p.getPPid(pidNumber)
     print "Parent pid number cmdline is: %s" % p.getPPidCmdline(pidNumber)
     print "Parent parent pid number is: %s" % p.getPPPid(pidNumber)
     print "Pid chain is: %s" % p.getPidChain(pidNumber)
     print "Pid Cmdline chain is: %s" % p.getCmdlineChain(pidNumber)
-    print "Pid number ContainerId is %s" % p.getContainerId(pidNumber)
+
+    # TODO(Guodong Ding) pid should use 'docker-proxy's' pid.
+    # print "Pid number's socket inode number is %s" % p.getFDSocketInodeNumber(pidNumber)
+    # print "Pid number's port number is %s" % p.getPortNumberWithInodeNumber(pidNumber)
+    # print "Pid number ContainerId is %s" % p.getContainerId(pidNumber)
+    # print "Pid number ContainerShortId is %s" % p.getContainerShortIDWithPort(pidNumber)
+
     print "Pid number Container info is %s" % p.getContainerInfo(pidNumber)
