a3a3c23ce65c750f937b1f62c97f1c78861de568
DingGuodong <uberurey_ups@163.com>
2016-11-14 14:42:43 +0800

    add a database_error_decorator for view

---
+++ functions/log/debug_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File:               LinuxBashShellScriptForOps:debug_example.py
+User:               Guodong
+Create Date:        2016/11/8
+Create Time:        11:56
+ """
+from debug import log
+
+log.log().info('xx')
