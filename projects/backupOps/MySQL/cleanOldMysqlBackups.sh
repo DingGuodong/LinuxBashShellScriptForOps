c6fbc51aa14ce06e9ca80157cc104baeac1f6104
DingGuodong <uberurey_ups@163.com>
2017-04-12 18:29:16 +0800

    bug fix: some old backups can not deleted in crontab job.

--- projects/backupOps/MySQL/cleanOldMysqlBackups.sh
+++ projects/backupOps/MySQL/cleanOldMysqlBackups.sh
@@ -2,9 +2,10 @@
 mysql_backup_dir=/data/backup/db/mysql
 save_old_backups_for_days=5
 save_days=${save_old_backups_for_days:-10}
-need_clean=$(find ${mysql_backup_dir} -maxdepth 1 -mtime +${save_days} -exec ls '{}' \;)
+# use -ctime to replace -mtime, replace -mtime with -ctime option
+need_clean=$(find ${mysql_backup_dir} -maxdepth 1 -ctime +${save_days} -exec ls '{}' \;)
 if [ "x${need_clean}" != "x" ]; then
-    find ${mysql_backup_dir} -maxdepth 1 -mtime +${save_days} -exec rm -rf '{}' \;
+    find ${mysql_backup_dir} -maxdepth 1 -ctime +${save_days} -exec rm -rf '{}' \;
     echo "old backups have been cleaned! "
 else
     echo "nothing can be cleaned, skipped! "
