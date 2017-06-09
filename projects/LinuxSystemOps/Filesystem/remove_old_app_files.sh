150b16f43066bbb0165b2ae2dc346e4bb976538c
DingGuodong <uberurey_ups@163.com>
2017-06-09 09:06:49 +0800

    critical bug fix

--- projects/LinuxSystemOps/Filesystem/remove_old_app_files.sh
+++ projects/LinuxSystemOps/Filesystem/remove_old_app_files.sh
@@ -1,26 +1,38 @@
 #!/usr/bin/env bash
 # remove old app files, not restrict, NO QA
-set -o errexit
+#set -o errexit
+#set -o xtrace
 apps_dir="/opt/ebt/apps"
 current_pwd=$(pwd)
 if [[ ${current_pwd} != ${apps_dir} ]]; then
-    cd apps_dir
+    cd ${apps_dir}
 fi
 app_list="
-appname1
-appname2
-appname3
+agent-management
+dataexchange
+ebtdatres
+erisk
+erp
+policy
+proposal
+user
 "
 
 current_app_list=$(ls -1 .)
 
+# IFS=' '$'\t'$'\n', IFS=$' \t\n', If IFS is unset, or its value is exactly <space><tab><newline>
+old_IFS=$IFS
+IFS=' '$'\t'$'\n'
+
 for app in ${app_list};do
-    if [[ ${current_app_list} =~ ${app} ]]; then # if echo "${strings}" | grep ${string}; then
-        app_count=$(ls -t . | grep ${app}| wc -l)
-        if [[ ${app_count} > 2 ]]; then
-            app_count=$(expr ${app_count} - 2 ) # app_count=$((app_count-2))
+    if echo "$current_app_list" | grep ${app} >/dev/null 2>&1; then #if [[ ${current_app_list} =~ ${app} ]]; then
+        all_app_count=$(ls -t . | grep ${app}| wc -l)
+        if [[ ${all_app_count} > 2 ]]; then
+            old_app_count=$(expr ${all_app_count} - 2 ) # old_app_count=$((all_app_count-2))
+            echo "Found:$app, Old files count:$old_app_count"
+            ls -t . | grep ${app} | tail -${old_app_count} | xargs rm -rf
         fi
-        echo "Found:$app, Old files count:$app_count"
-        ls -t . | grep ${app} | tail -${app_count} | xargs rm -rf
     fi
 done
+
+IFS="$old_IFS"
