c876d598f3cc637315c7b02dfd88dd4aa5fa8be7
DingGuodong <uberurey_ups@163.com>
2017-06-14 15:37:41 +0800

    python pickling(serialization)

---
+++ functions/string/picklingSerialization/picklingWithJson.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File:               LinuxBashShellScriptForOps:picklingWithJson.py
+User:               Guodong
+Create Date:        2017/6/14
+Create Time:        15:30
+ """
+import json
+
+
+class Student(object):
+    def __init__(self, name, age, score):
+        self.name = name
+        self.age = age
+        self.score = score
+
+
+def student2dict(student):
+    return {
+        'name': student.name,
+        'age': student.age,
+        'score': student.score
+    }
+
+
+s = Student('Bob', 20, 88)
+
+class2json = json.dumps(s, indent=4, default=student2dict)
+
+print class2json
---
+++ functions/string/picklingSerialization/picklingWithPickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File:               LinuxBashShellScriptForOps:picklingWithPickle.py
+User:               Guodong
+Create Date:        2017/6/14
+Create Time:        15:28
+ """
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+import requests
+
+url = "http://coolshell.cn/feed"
+
+payload = ""
+headers = {
+    'cache-control': "no-cache",
+}
+
+response = requests.request("GET", url, data=payload, headers=headers)
+
+xml_text = response.text
+
+with open('somefile', 'wb') as f:
+    pickle.dump(xml_text, f)
+
+with open('somefile', 'rb') as f:
+    print pickle.load(f)
