7ae75c6225fd2c9dcfef3d770b9f777a4cde429e
DingGuodong <uberurey_ups@163.com>
2017-07-06 18:23:22 +0800

    Linux curl examples, sleep random seconds

---
+++ functions/net/tcp/http/curlOps.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Created by PyCharm.
+# File:                 LinuxBashShellScriptForOps:${NAME}.sh
+# User:                 Guodong
+# Create Date:          2017/7/6
+# Create Time:          16:33
+# Function:             
+# Note:                 
+# Prerequisite:         
+# Description:          Linux curl examples, sleep random seconds
+# Reference:
+
+url_list="
+www.baidu.com
+www.aliyun.com
+www.163.com
+"
+for url in ${url_list}; do
+    curl -sL -w "%{http_code} %{size_download}\\n" ${url} -o /dev/null
+done
+
+
+# or
+# Note: The file must use UNIX and OS X(LF, \n) line ending(Know as 'Line separators') not Windows(CRLF, \r\n),
+# you can use 'dos2unix -n url.txt url_unix.txt' to change it easily,
+# use 'dos2unix url.txt' will convert in old file mode
+for url in `cat url.txt`; do  # for url in $(cat url.txt); do
+    curl -sL -w "%{http_code} %{size_download}\\n" ${url} -o /dev/null
+    sleep $(($RANDOM % 7 + 3 ))
+done
