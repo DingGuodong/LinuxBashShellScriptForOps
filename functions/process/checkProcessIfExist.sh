a4eecb561aa151f69af18e77b68fe056da8b2a09
DingGuodong <uberurey_ups@163.com>
2017-07-14 13:54:30 +0800

    add message about "kill -0 <pid>", see man 2 kill

--- functions/process/checkProcessIfExist.sh
+++ functions/process/checkProcessIfExist.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 PIDFILE=""
-if ! kill -0 $(cat ${PIDFILE}) >/dev/null 2>&1; then
-    echo xxx
+# about "kill -0 <pid>", see man 2 kill
+# If sig is 0, then no signal is sent, but error checking is still performed;
+# this can be used to check for the existence of a process ID or process group ID.
+if test -s  ${PIDFILE}; then
+    if ! kill -0 $(cat ${PIDFILE}) >/dev/null 2>&1; then
+        echo xxx
+    fi
+else
+     echo "PID file could not be found!"
+     exit 1
 fi
-
