05df5907ff3ae432b0e885e7d921bac65172372b
DingGuodong <uberurey_ups@163.com>
2017-07-19 16:40:25 +0800

    ask user yes or no to confirm or continue in bash, improvement

--- functions/options/askUserYesNoToConfirmOrContinue.sh
+++ functions/options/askUserYesNoToConfirmOrContinue.sh
@@ -10,7 +10,13 @@
 # Description:          ask user yes or no to confirm or continue in bash
 # Reference:            
 
-read -p "Enter the y or Y to continue:" user_answer
+read -n1 -r -p "Enter the y or Y to continue:" user_answer  # read -n1 -r -p "Press any key to continue..." key
 if [ "${user_answer}" != "y" ] && [ "${user_answer}" != "Y" ]; then
+    echo
     exit 1
+else
+    echo
 fi
+
+# main job
+echo "foo"
