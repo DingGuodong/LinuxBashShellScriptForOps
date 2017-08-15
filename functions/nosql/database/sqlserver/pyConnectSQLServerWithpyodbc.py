5cc3b132a6170420241e48292c6004ab38917186
DingGuodong <uberurey_ups@163.com>
2017-08-15 10:12:00 +0800

    Python Connect to SQL Server on Windows With pyodbc

--- functions/nosql/database/sqlserver/pyConnectSQLServerWithpyodbc.py
+++ functions/nosql/database/sqlserver/pyConnectSQLServerWithpyodbc.py
@@ -3,11 +3,11 @@
 # -*- coding: utf8 -*-
 """
 Created by PyCharm.
-File:               LinuxBashShellScriptForOps:pyConnectSQLServer.py
+File:               LinuxBashShellScriptForOps:pyConnectSQLServerWithpyodbc.py
 User:               Guodong
 Create Date:        2017/8/15
 Create Time:        9:04
-Description:        Python Connect to SQL Server on Windows
+Description:        Python Connect to SQL Server on Windows With pyodbc
 References:         Create Python apps using SQL Server on Windows
                     https://www.microsoft.com/en-us/sql-server/developer-get-started/python/windows/
                     https://www.microsoft.com/en-us/sql-server/developer-get-started/python/windows/step/2.html
@@ -16,27 +16,32 @@ References:         Create Python apps using SQL Server on Windows
 
  """
 import pyodbc
-import datetime
+from datetime import datetime
 
+driver = 'ODBC Driver 13 for SQL Server'
 server = 'localhost'
+port = 1134
 database = 'SampleDB'
 username = 'sa'
 password = 'your_password'
 cnxn = pyodbc.connect(
-    'DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + server + ';PORT=1443;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    r'DRIVER={driver};SERVER={server};PORT={port};DATABASE={database};UID={username};PWD={password}'.format(
+        driver=driver, server=server, port=port, database=database, username=username, password=password)
+)
+
 cursor = cnxn.cursor()
 
 print ('Inserting a new row into table')
 # Insert Query
 tsql = "INSERT INTO Employees (Name, Location) VALUES (?,?);"
 with cursor.execute(tsql, 'Jake', 'United States'):
-    print ('Successfuly Inserted!')
+    print ('Successfully Inserted!')
 
 # Update Query
 print ('Updating Location for Nikita')
 tsql = "UPDATE Employees SET Location = ? WHERE Name = ?"
 with cursor.execute(tsql, 'Sweden', 'Nikita'):
-    print ('Successfuly Updated!')
+    print ('Successfully Updated!')
 
 # Delete Query
 print ('Deleting user Jared')
