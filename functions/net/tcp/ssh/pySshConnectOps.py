dddda651e1bff280242fec437001f4e78bdca178
DingGuodong <uberurey_ups@163.com>
2017-09-12 17:06:41 +0800

    raise a paramiko add customized environment issue

--- functions/net/tcp/ssh/pySshConnectOps.py
+++ functions/net/tcp/ssh/pySshConnectOps.py
@@ -12,8 +12,12 @@ import paramiko
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect("10.6.28.36", port=20122, username="root", key_filename="id_rsa", timeout=2)
-stdin, stdout, stderr = client.exec_command("uname -a")
+client.connect("120.27.194.133", port=22, username="root",
+               key_filename="C:\Users\Guodong\.ssh\ebt-linux-centos-ssh-root-key.pem", timeout=5)
+
+# TODO(Guodong Ding) paramiko add customized environment issue, next line not works on centos and ubuntu
+stdin, stdout, stderr = client.exec_command('echo "x$user_defined_variable_name"',
+                                            environment={"user_defined_variable_name": "1"}, get_pty=True)
 for line in stdout:
     print line,
 client.close()
