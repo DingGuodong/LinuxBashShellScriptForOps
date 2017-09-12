67b7da6a48611cea05d51119e7390a1b7322c347
DingGuodong <uberurey_ups@163.com>
2017-09-12 18:53:14 +0800

    Run system command is better than Fabric and Paramiko

---
+++ functions/net/tcp/ssh/pySshInteractiveShell.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File:               LinuxBashShellScriptForOps:pySshInteractiveShell.py
+User:               Guodong
+Create Date:        2017/9/12
+Create Time:        17:25
+Description:
+References:         Fabric is better for interactive shell than paramiko.
+                    Run system command is better than Fabric.
+                    All in all, Run system command is better than Fabric and Paramiko.
+Prerequisites:      []
+ """
