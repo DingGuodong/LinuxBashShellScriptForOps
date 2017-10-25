f68b1062d00761e3637fdc9cfa611f943c47cfd8
DingGuodong <uberurey_ups@163.com>
2017-10-25 20:07:47 +0800

    bug fixed, support more Windows System version.

--- functions/system/getSystemVersion.py
+++ functions/system/getSystemVersion.py
@@ -8,12 +8,13 @@ User:               Guodong
 Create Date:        2016/12/16
 Create Time:        14:51
  """
-import sys
+import codecs
+import locale
 import os
 import platform
 import subprocess
-import codecs
-import locale
+import sys
+import time
 
 
 def get_system_encoding():
@@ -25,7 +26,8 @@ def get_system_encoding():
     try:
         encoding = locale.getdefaultlocale()[1] or 'ascii'
         codecs.lookup(encoding)
-    except Exception:
+    except Exception as _:
+        del _
         encoding = 'ascii'
     return encoding
 
@@ -35,26 +37,54 @@ DEFAULT_LOCALE_ENCODING = get_system_encoding()
 mswindows = (sys.platform == "win32")  # learning from 'subprocess' module
 linux = (sys.platform == "linux2")
 
-hidden_hostname = True
+hidden_hostname = False
 
 if mswindows:
     uname = list(platform.uname())
     if hidden_hostname:
         uname[1] = "hidden_hostname"
-    print uname
+    print tuple(uname)
 
     import _winreg
 
     try:
         reg_key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
         if reg_key:
-            ProductName = _winreg.QueryValueEx(reg_key, "ProductName")[0] or None
-            EditionId = _winreg.QueryValueEx(reg_key, "EditionId")[0] or None
-            ReleaseId = _winreg.QueryValueEx(reg_key, "ReleaseId")[0] or None
-            CurrentBuild = _winreg.QueryValueEx(reg_key, "CurrentBuild")[0] or None
-            BuildLabEx = _winreg.QueryValueEx(reg_key, "BuildLabEx")[0][:9] or None
-            print (ProductName, EditionId, ReleaseId, CurrentBuild, BuildLabEx)
+            InstallationType = _winreg.QueryValueEx(reg_key, "InstallationType")[0] or ""
+            if InstallationType and InstallationType == 'Client':
+                InstallDate = time.strftime('%Y-%m-%d %H:%M:%S',
+                                            time.localtime(_winreg.QueryValueEx(reg_key, "InstallDate")[0]))
+                ProductName = _winreg.QueryValueEx(reg_key, "ProductName")[0] or None
+                EditionId = _winreg.QueryValueEx(reg_key, "EditionId")[0] or None
+                ReleaseId = _winreg.QueryValueEx(reg_key, "ReleaseId")[0] or None
+                CurrentBuild = _winreg.QueryValueEx(reg_key, "CurrentBuild")[0] or None
+                CurrentVersion = _winreg.QueryValueEx(reg_key, "CurrentVersion")[0] or None
+                BuildLabEx = _winreg.QueryValueEx(reg_key, "BuildLabEx")[0][:9] or None
+                ProductId = _winreg.QueryValueEx(reg_key, "ProductId")[0] or None
+                print (
+                    InstallDate, ProductName, EditionId, ReleaseId, CurrentVersion, CurrentBuild, BuildLabEx, ProductId)
+            elif InstallationType and InstallationType == 'Server':
+                InstallDate = time.strftime('%Y-%m-%d %H:%M:%S',
+                                            time.localtime(_winreg.QueryValueEx(reg_key, "InstallDate")[0]))
+                ProductName = _winreg.QueryValueEx(reg_key, "ProductName")[0] or None
+                EditionId = _winreg.QueryValueEx(reg_key, "EditionId")[0] or None
+                CurrentVersion = _winreg.QueryValueEx(reg_key, "CurrentVersion")[0] or None
+                CurrentBuild = _winreg.QueryValueEx(reg_key, "CurrentBuild")[0] or None
+                BuildLabEx = _winreg.QueryValueEx(reg_key, "BuildLabEx")[0][:9] or None
+                if CurrentVersion <= '6.1':  # 6.1 include 'Windows Server 2008 R2(6.1)'
+                    print (InstallDate, ProductName, EditionId, CurrentVersion, CurrentBuild, BuildLabEx)
+                else:
+                    ProductId = _winreg.QueryValueEx(reg_key, "ProductId")[
+                                    0] or None  # Windows Server 2008 R2 is not supported
+                    print (InstallDate, ProductName, EditionId, CurrentVersion, CurrentBuild, BuildLabEx, ProductId)
+    except WindowsError as e:
+        print 'WindowsError is captured.'
+        print e
+        print e.args
+        print e.message.decode(DEFAULT_LOCALE_ENCODING)
     except Exception as e:
+        print e
+        print e.args
         print e.message.decode(DEFAULT_LOCALE_ENCODING)
 
 if linux:
