fe536254e13fdd35840c5d035b47458650b438f0
DingGuodong <uberurey_ups@163.com>
2017-10-30 20:54:14 +0800

    fix small bug

--- projects/LinuxSystemOps/SoftwareManagement/nginx/retrieveLatestPcreVersionUrl.py
+++ projects/LinuxSystemOps/SoftwareManagement/nginx/retrieveLatestPcreVersionUrl.py
@@ -12,9 +12,10 @@ retrieve, fetch, find latest version of PCRE URL
 
  """
 
+import re
+
 import requests
 from bs4 import BeautifulSoup
-import re
 
 headers = {
     'Cache-Control': 'max-age=0',
@@ -27,7 +28,7 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh-TW;q=0.4'
 }
 
-downloads_page_url = 'http://zlib.net/'
+downloads_page_url = 'https://ftp.pcre.org/pub/pcre/'
 file_url_list = list()
 latest_version = ""
 file_url = ""
@@ -37,9 +38,9 @@ Soup = BeautifulSoup(content.text, 'lxml')
 available_version = Soup.find_all('a')
 
 for link in available_version:
-    pattern = re.compile('zlib-.*\.tar\.gz$')
+    pattern = re.compile('pcre-.*\.tar\.gz$')
     match = pattern.search(link.get("href"))
     if match:
         file_url_list.append(downloads_page_url + match.group())
 
-print file_url_list[0]
+print file_url_list[-1]
