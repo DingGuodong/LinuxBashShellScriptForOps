5d3e097fc7362d0a8943fd30dc54914c3e35daad
DingGuodong <uberurey_ups@163.com>
2018-01-11 11:37:04 +0800

    use map as parallel executor to improve performance

---
+++ functions/performance/other-performance-improvement/ReadMe.md
@@ -0,0 +1,6 @@
+# THIS DIRECTORY CONTAINS THOSE CONTENTS AS FOLLOWS
+> Note: mainly for Python coding, some shell coding as well 
+- performance tuning
+- performances improvements
+- code best practice for performance tuning
+- some algorithm related
---
+++ functions/performance/other-performance-improvement/pyUseMapImprovePerformance.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:pyUseMapImprovePerformance.py
+Version:                0.0.1
+Author:                 Guodong
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2018/1/11
+Create Time:            10:32
+Description:            use map as parallel executor to improve performance
+Long Description:       
+References:             https://yq.aliyun.com/articles/337746?spm=5176.100238.spm-cont-list.121.4b8421e547hM0C
+Prerequisites:          []
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+
+import os
+import sys
+from multiprocessing import Pool
+
+try:
+    # PIL is only 32-bit available on 64-bit Windows System
+    from PIL import Image
+except ImportError as e:
+    print e.args
+    sys.exit(1)
+
+SIZE = (75, 75)
+SAVE_DIRECTORY = 'thumbs'
+
+
+def get_image_paths(folder):
+    return [os.path.join(folder, f) for f in os.listdir(folder) if 'jpeg' in f.lower() or 'jpg' in f.lower()]
+
+
+def create_thumbnail(filename):
+    im = Image.open(filename)
+    im.thumbnail(SIZE, Image.ANTIALIAS)
+    base, fname = os.path.split(filename)
+    save_path = os.path.join(base, SAVE_DIRECTORY, fname)
+    im.save(save_path)
+
+
+def example_another():
+    import urllib2
+    from multiprocessing.dummy import Pool as ThreadPool
+
+    urls = [
+        'http://www.python.org',
+        'http://www.python.org/about/',
+        'http://www.onlamp.com/pub/a/python/2003/04/17/metaclasses.html',
+        'http://www.python.org/doc/',
+        'http://www.python.org/download/',
+        'http://www.python.org/getit/',
+        'http://www.python.org/community/',
+        'https://wiki.python.org/moin/',
+        'http://planet.python.org/',
+        'https://wiki.python.org/moin/LocalUserGroups',
+        'http://www.python.org/psf/',
+        'http://docs.python.org/devguide/',
+        'http://www.python.org/community/awards/'
+        # etc..
+    ]
+    # Make the Pool of workers
+    thread_pool = ThreadPool(4)
+    # Open the urls in their own threads
+    # and return the results
+    results = thread_pool.map(urllib2.urlopen, urls)
+    # close the pool and wait for the work to finish
+    thread_pool.close()
+    thread_pool.join()
+
+    print results
+
+
+if __name__ == '__main__':
+    original_images_folder = os.path.abspath(unicode(r"D:\Users\Chris\Pictures\iPhone\DCIM\100APPLE"))
+    save_dirs = os.path.join(original_images_folder, SAVE_DIRECTORY)
+    if not os.path.exists(save_dirs):
+        os.mkdir(save_dirs)
+    images = get_image_paths(original_images_folder)
+    print "{count} images files are going to be processed ...".format(count=len(images))
+    pool = Pool()
+    pool.map(create_thumbnail, images)
+    pool.close()
+    pool.join()
