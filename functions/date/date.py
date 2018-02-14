8897b87cf0120bad172fc8c753dde6a8ebf83e12
DingGuodong <uberurey_ups@163.com>
2018-02-14 09:55:01 +0800

    add some useful functions about date time

--- functions/date/date.py
+++ functions/date/date.py
@@ -1,11 +1,46 @@
 # encoding: utf-8
 # -*- coding: utf8 -*-
+import calendar
 import datetime
 import sys
 import time
 
 import delorean
 import pytz
+from dateutil.relativedelta import relativedelta
+
+# Define the constants
+SECONDS_PER_MINUTE = 60
+SECONDS_PER_HOUR = 3600
+SECONDS_PER_DAY = 86400
+
+# 'Wed Feb 14 09:25:49 2018'
+print(time.asctime())
+# local structure time
+print(time.localtime())
+# GMT structure time
+print(time.gmtime())
+
+# Convert seconds into GMT date
+print(time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime(time.time())))
+
+# Python's program to get current time MST EST UTC GMT HST
+
+
+mst = pytz.timezone('MST')
+print("Time in MST:", datetime.datetime.now(mst))
+
+est = pytz.timezone('EST')
+print("Time in EST:", datetime.datetime.now(est))
+
+utc = pytz.timezone('UTC')
+print("Time in UTC:", datetime.datetime.now(utc))
+
+gmt = pytz.timezone('GMT')
+print("Time in GMT:", datetime.datetime.now(gmt))
+
+hst = pytz.timezone('HST')
+print("Time in HST:", datetime.datetime.now(hst))
 
 print time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
 print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
@@ -39,6 +74,9 @@ print datetime.datetime.fromtimestamp(1471932539.15, pytz.timezone('Asia/Shangha
 
 # Time to Unix timestamp
 print time.mktime(time.strptime('2016-08-23 14:08:01', '%Y-%m-%d %H:%M:%S'))
+# # Saturday, October 10, 2015 10:10:00 AM
+# year, month, day, hour=None, minute=None, second=None, microsecond=None, tzinfo=None
+print("Unix Timestamp: ", (time.mktime(datetime.datetime(2015, 10, 10, 10, 10).timetuple())))
 
 # Time zone support
 print delorean.Delorean(timezone="Asia/Shanghai")
@@ -49,3 +87,65 @@ print delorean.Delorean(timezone="Asia/Shanghai").start_of_day
 print delorean.Delorean(timezone="Asia/Shanghai").end_of_day
 
 # 20161229235959Z, Z代表0时区，或者叫UTC统一时间。
+
+# refers to http://www.pythonprogramming.in/date-time.html
+# Python's program to calculate time difference between two datetime objects.
+datetimeFormat = '%Y-%m-%d %H:%M:%S.%f'
+date1 = '2016-04-16 10:01:28.585'
+date2 = '2016-03-10 09:56:28.067'
+diff = datetime.datetime.strptime(date1, datetimeFormat) \
+       - datetime.datetime.strptime(date2, datetimeFormat)
+print("Difference:", diff)
+print("Days:", diff.days)
+print("Microseconds:", diff.microseconds)
+print("Seconds:", diff.seconds)
+
+# Python's program to display all dates between two dates.
+start = datetime.datetime.strptime("2016-05-30", "%Y-%m-%d")
+end = datetime.datetime.strptime("2016-06-02", "%Y-%m-%d")
+date_array = \
+    (start + datetime.timedelta(days=x) for x in range(0, (end - start).days))
+for date_object in date_array:
+    print(date_object.strftime("%Y-%m-%d"))
+
+# Python's program to add N year month day hour min sec to date.
+add_days = datetime.datetime.today() + relativedelta(days=+6)
+add_months = datetime.datetime.today() + relativedelta(months=+6)
+add_years = datetime.datetime.today() + relativedelta(years=+6)
+add_hours = datetime.datetime.today() + relativedelta(hours=+6)
+add_mins = datetime.datetime.today() + relativedelta(minutes=+6)
+add_seconds = datetime.datetime.today() + relativedelta(seconds=+6)
+print("Current Date Time:", datetime.datetime.today())
+print("Add 6 days:", add_days)
+print("Add 6 months:", add_months)
+print("Add 6 years:", add_years)
+print("Add 6 hours:", add_hours)
+print("Add 6 mins:", add_mins)
+print("Add 6 seconds:", add_seconds)
+
+# Python's program to subtract N year month day hour min sec to date.
+sub_days = datetime.datetime.today() + relativedelta(days=-6)
+sub_months = datetime.datetime.today() + relativedelta(months=-6)
+sub_years = datetime.datetime.today() + relativedelta(years=-6)
+sub_hours = datetime.datetime.today() + relativedelta(hours=-6)
+sub_mins = datetime.datetime.today() + relativedelta(minutes=-6)
+sub_seconds = datetime.datetime.today() + relativedelta(seconds=-6)
+print("Current Date Time:", datetime.datetime.today())
+print("Subtract 6 days:", sub_days)
+print("Subtract 6 months:", sub_months)
+print("Subtract 6 years:", sub_years)
+print("Subtract 6 hours:", sub_hours)
+print("Subtract 6 mins:", sub_mins)
+print("Subtract 6 seconds:", sub_seconds)
+
+# Python's program to weekday of first day of the month and
+# number of days in month, for the specified year and month.
+print("Year:2002 - Month:2")
+month_range = calendar.monthrange(2002, 2)
+print("Weekday of first day of the month:", month_range[0])
+print("Number of days in month:", month_range[1])
+print()
+print("Year:2010 - Month:5")
+month_range = calendar.monthrange(2010, 5)
+print("Weekday of first day of the month:", month_range[0])
+print("Number of days in month:", month_range[1])
