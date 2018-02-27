6a80f9d21ed6360c55b16f5517a812381397ff01
DingGuodong <uberurey_ups@163.com>
2018-02-27 11:34:58 +0800

    add quick test script to check conn to Microsoft SQL Server

---
+++ functions/nosql/database/sqlserver/pyConnectSQLServerWithpymssqlQuickTest.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:pyConnectSQLServerWithpymssqlQuickTest.py
+Version:                0.0.1
+Author:                 Guodong
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2018/2/27
+Create Time:            11:10
+Description:            
+Long Description:       
+References:             https://docs.microsoft.com/en-us/sql/connect/python/pymssql/python-sql-driver-pymssql
+                        https://docs.microsoft.com/en-us/sql/connect/python/pymssql/step-3-proof-of-concept-connecting-to-sql-using-pymssql
+                        https://www.lfd.uci.edu/~gohlke/pythonlibs/#pymssql
+Prerequisites:          []
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+import pymssql
+from collections import Iterable
+
+server = 'rdsxxxx.sqlserver.rds.aliyuncs.com'
+port = '3433'
+database = 'database_name'
+username = 'username'
+password = 'password'
+
+try:
+    _ = pymssql.connect(server=server, port=port, user=username, password=password, database=database,
+                        login_timeout=10)
+except pymssql.DatabaseError as e:
+    if isinstance(e.args, Iterable):
+        for arg in e.args:
+            print(arg)
+    else:
+        print(e.args)
+except pymssql.InterfaceError as e:
+    if isinstance(e.args, Iterable):
+        for arg in e.args:
+            print(arg)
+    else:
+        print(e.args)
+else:
+    print("success")
---
+++ functions/nosql/database/sqlserver/pyConnectSQLServerWithpyodbcQuickTest.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:pyConnectSQLServerWithpyodbcQuickTest.py
+Version:                0.0.1
+Author:                 Guodong
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2018/2/26
+Create Time:            19:47
+Description:            
+Long Description:       
+References:             https://docs.microsoft.com/en-us/sql/connect/python/python-driver-for-sql-server
+                        https://docs.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server
+                        https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc
+Prerequisites:          pyodbc
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+import pyodbc
+from collections import Iterable
+
+# https://github.com/mkleehammer/pyodbc/wiki/Connecting-to-SQL-Server-from-Windows
+driver = '{ODBC Driver 17 for SQL Server}'
+# other not tested driver as follows:
+# {SQL Server} - released with SQL Server 2000
+# {SQL Native Client} - released with SQL Server 2005 (also known as version 9.0)
+# {SQL Server Native Client 10.0} - released with SQL Server 2008
+# {SQL Server Native Client 11.0} - released with SQL Server 2012
+# {ODBC Driver 11 for SQL Server} - supports SQL Server 2005 through 2014
+# {ODBC Driver 13 for SQL Server} - supports SQL Server 2005 through 2016
+# {ODBC Driver 17 for SQL Server}
+server = 'rdsxxxx.sqlserver.rds.aliyuncs.com'
+port = 3433
+database = 'database_name'
+username = 'username'
+password = 'password'
+
+try:
+    _ = pyodbc.connect(
+        r'DRIVER={driver};SERVER={server},{port};DATABASE={database};UID={username};PWD={password}'.format(
+            driver=driver, server=server, port=port, database=database, username=username, password=password),
+        timeout=10  # timeout maybe not works
+    )
+except pyodbc.DatabaseError as e:
+    if isinstance(e.args, Iterable):
+        for arg in e.args:
+            print(arg)
+    else:
+        print(e.args)
+except pyodbc.InterfaceError as e:
+    if isinstance(e.args, Iterable):
+        for arg in e.args:
+            print(arg)
+    else:
+        print(e.args)
+else:
+    print("success")
