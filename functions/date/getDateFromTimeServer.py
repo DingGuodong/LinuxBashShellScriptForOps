21575be5774f9921b038f3b9ee185f68c4e396cf
guodong <uberurey_ups@163.com>
2018-04-25 16:23:43 +0800

    update date time functions

--- functions/date/getDateFromTimeServer.py
+++ functions/date/getDateFromTimeServer.py
@@ -15,6 +15,9 @@ except ImportError:
     finally:
         import ntplib
 
+mswindows = (sys.platform == "win32")  # learning from 'subprocess' module
+linux = (sys.platform == "linux2")
+
 tz = time.strftime("%Z", time.localtime(time.time())).decode(encoding=sys.getfilesystemencoding()).encode('utf-8')
 print "当前时区 ==> %s" % tz
 
@@ -22,10 +25,20 @@ server = 'pool.ntp.org'  # time.nist.gov  time-nw.nist.gov
 
 c = ntplib.NTPClient()
 r = c.request(server)
-t = r.tx_time
+t = r.tx_time  # Transmit timestamp in system time.
 
 time_in_server = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t))
 time_in_local = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
 if r.offset > 15:
+    print "当前系统时间与服务器时间相差15s以上"
     print "当前系统时间 => %s" % time_in_local
     print "服务器时间 => %s" % time_in_server
+    # setting system date and time
+    _date = time.strftime('%Y-%m-%d', time.localtime(t))
+    _time = time.strftime('%X', time.localtime(t))
+    if mswindows:
+        os.system('date {} && time {}'.format(_date, _time))
+    elif linux:
+        os.system("date -s \"%s %s\" && hwclock -w" % (_date, _time))
+    else:
+        print("你也许现在想修改系统时间")
--- functions/date/getDateFromWebHost.py
+++ functions/date/getDateFromWebHost.py
@@ -2,23 +2,18 @@
 import urllib2
 import datetime
 import time
-
-# import os
+import certifi
 
 host = 'https://www.baidu.com'
 request = urllib2.Request(host)
-response = urllib2.urlopen(request)
+response = urllib2.urlopen(request, timeout=10, cafile=certifi.where())  # socket.setdefaulttimeout(10)
 date = response.info().getheader('Date')
-print date
-# Thu, 16 Jun 2016 01:49:52 GMT
+
+print(date)  # Thu, 16 Jun 2016 01:49:52 GMT
+
 # https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
 time_header = datetime.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')
 
-# print time.strptime(date, '%a, %d %b %Y %H:%M:%S %Z')
+print(time_header.now())
 
-print time_header
-
-print datetime.datetime.now()
-
-time_tuple = time.mktime(time_header.timetuple())
-print time.strftime('%Y-%m-%d %H:%M:%S', time_header.timetuple())
+print(time.strftime('%Y-%m-%d %H:%M:%S', time_header.now().timetuple()))
