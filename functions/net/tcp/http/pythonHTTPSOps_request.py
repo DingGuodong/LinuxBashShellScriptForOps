5fee488dc6336260816adc7d8eee943734816e8a
DingGuodong <uberurey_ups@163.com>
2018-05-03 11:12:29 +0800

    update request example, add encoding

--- functions/net/tcp/http/pythonHTTPSOps_request.py
+++ functions/net/tcp/http/pythonHTTPSOps_request.py
@@ -10,11 +10,26 @@ Create Time:        11:40
  """
 import requests
 
-# http://docs.python-requests.org/en/master/
-# http://docs.python-requests.org/en/master/user/quickstart/#make-a-request
-r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
-print r.status_code
-print r.headers['content-type']
-print r.encoding
-print r.text
-print r.json()
+
+def example1():
+    # http://docs.python-requests.org/en/master/
+    # http://docs.python-requests.org/en/master/user/quickstart/#make-a-request
+    r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+    print r.status_code
+    print r.headers['content-type']
+    print r.encoding
+    print r.text.encode(r.encoding)
+    print r.json()
+
+
+def example2():
+    url_with_302_307 = 'http://www.baidu.com/'
+    response = requests.request('GET', url_with_302_307, allow_redirects=True)  # Defaults to ``True``.
+    print(response.status_code)
+    print(response.encoding)
+    if response.ok:
+        print(response.text.encode(response.encoding))
+
+
+if __name__ == '__main__':
+    example2()
