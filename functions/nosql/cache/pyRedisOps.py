fcd844ef4a05a04b519fc6684572cd8c5294f101
DingGuodong <uberurey_ups@163.com>
2018-05-17 14:55:44 +0800

    Redis client basic operations

--- functions/nosql/cache/pyRedisOps.py
+++ functions/nosql/cache/pyRedisOps.py
@@ -7,15 +7,22 @@ File:               LinuxBashShellScriptForOps:pyRedisOps.py
 User:               Guodong
 Create Date:        2016/9/7
 Create Time:        10:36
+
+Redis client basic operations
+
  """
 
 import redis
 
-redis_host = "10.6.28.46"
+redis_host = "127.0.0.1"
 redis_port = 6379
-redis_password = "6d78247b460acc6bf1d9263e14382fea"
+redis_password = ""
 
-c = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password)
-c.set("a", "1")
-c.set("a", "2", nx=True)
-print c.get("a")
+try:
+    c = redis.StrictRedis(host=redis_host, port=redis_port, password=redis_password, socket_timeout=5)
+    role = c.info().get('role')
+    if not c.exists('redis_role_status') or c.get('redis_role_status') != role:
+        c.set('redis_role_status', role)
+    print(c.get('redis_role_status'))
+except (redis.exceptions.ConnectionError, redis.exceptions.TimeoutError, redis.exceptions.ResponseError) as e:
+    print(e.message)
