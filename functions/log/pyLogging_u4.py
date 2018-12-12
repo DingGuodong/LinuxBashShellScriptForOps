6f686cabb78420d1cf0f8c1b1a56490e26d6d599
DingGuodong <uberurey_ups@163.com>
2018-12-12 10:05:40 +0800

    better logging support for large project

---
+++ functions/log/pyLogging_u4.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:pyLogging_u4.py.py
+Version:                0.0.1
+Author:                 Guodong
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2018/12/11
+Create Time:            14:27
+Description:            better logging support for large project
+Long Description:       we can add this part of code into __init__.py for project
+References:             
+Prerequisites:          []
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+
+import logging
+from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
+
+logger = logging.getLogger('mylog')
+
+
+def set_file_logger(filename, name="mylog", level=logging.INFO, format_string=None):
+    global logger
+    if not format_string:
+        format_string = "%(asctime)s %(name)s %(levelname)s %(thread)d : %(message)s"
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    file_handler = logging.FileHandler(filename)
+    file_handler.setLevel(level)
+    formatter = logging.Formatter(format_string, datefmt=None)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+def set_file_logger_date(filename, name="mylog", saves=10, level=logging.INFO, format_string=None):
+    global logger
+    if not format_string:
+        format_string = "%(asctime)s %(name)s %(levelname)s %(thread)d : %(message)s"
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    file_handler = TimedRotatingFileHandler(filename, when='d', backupCount=saves, )
+    file_handler.setLevel(level)
+    formatter = logging.Formatter(format_string, datefmt=None)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+def set_file_logger_size(filename, name="mylog", max_size=1024 * 1024 * 2, saves=10, level=logging.INFO,
+                         format_string=None):
+    global logger
+    if not format_string:
+        format_string = "%(asctime)s %(name)s %(levelname)s %(thread)d : %(message)s"
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    file_handler = RotatingFileHandler(filename, maxBytes=max_size, backupCount=saves)
+    file_handler.setLevel(level)
+    formatter = logging.Formatter(format_string, datefmt=None)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+
+def set_stream_logger(name='mylog', level=logging.DEBUG, format_string=None):
+    """
+    stream logger for debug purpose
+    """
+    global logger
+    if not format_string:
+        format_string = "%(asctime)s %(name)s %(levelname)s %(pathname)s:%(lineno)d " \
+                        "%(process)d %(thread)d : %(message)s"  # see logging.__init__.Formatter()
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+    formatter = logging.Formatter(format_string, datefmt=None)  # the ISO8601 date format, 2018-12-11 15:01:17,290
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+
+set_stream_logger('mylog', logging.INFO)
+
+if __name__ == '__main__':
+    logger.info("some msg here")
+
+    # using unicode or str with 'utf-8' for non-ascii character
+    logger.info(u"打印一些日志")
+    logger.info("打印一些日志".decode('utf-8'))  # 'utf-8' same with '-*- coding: utf-8 -*-', they must be same
