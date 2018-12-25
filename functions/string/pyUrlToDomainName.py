f84b490a07b1ce1e7f0e6ffc26e322067ddb07b9
DingGuodong <uberurey_ups@163.com>
2018-12-25 18:56:47 +0800

    get protocol host name from url, remove deprecated code

--- functions/string/pyUrlToDomainName.py
+++ functions/string/pyUrlToDomainName.py
@@ -12,9 +12,8 @@ Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOp
 Create Date:            2017/10/31
 Create Time:            11:17
 Description:            URL Address to Domain Name, Python get domain name from URL
-Long Description:       
+Long Description:       get protocol host name from url
 References:             https://stackoverflow.com/questions/9626535/get-domain-name-from-url
-                        https://ashiknesin.com/blog/get-domain-name-url-string-python/
 Prerequisites:          []
 Development Status:     3 - Alpha, 5 - Production/Stable
 Environment:            Console
@@ -28,25 +27,23 @@ Topic:                  Utilities
  """
 
 
-def get_domain_name_from_url(url):
+def get_domain_name(url):
     # https://stackoverflow.com/questions/9626535/get-domain-name-from-url
-    # https: // docs.python.org / 2 / library / urlparse.html
     from urlparse import urlparse
     parsed_uri = urlparse(url)
     # domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-    domain_name = "{uri.netloc}".format(uri=parsed_uri)
+    domain_name = "{uri.netloc}".format(uri=parsed_uri).split(":")[0]
     return domain_name
 
 
-def get_domain_name_from_url_alternative_method_1(url):
-    # https://ashiknesin.com/blog/get-domain-name-url-string-python/
-    import tldextract
-    extract_result = tldextract.extract(url)
-    domain_name = extract_result.subdomain + '.' + extract_result.domain + '.' + extract_result.suffix
-    return domain_name
+def get_domain_name_2(url):
+    import urllib
+    scheme, rest = urllib.splittype(url)
+    host, rest = urllib.splithost(rest)
+    return host.split(":")[0]
 
 
 if __name__ == '__main__':
-    url_to_test = 'http://xinbao.osap-saas.com/wss/OSAP'
-    print get_domain_name_from_url(url_to_test)
-    print get_domain_name_from_url_alternative_method_1(url_to_test)
+    url_to_test = 'https://www.gnu.org:443/software/bash/manual/bashref.html#Pipelines'
+    print get_domain_name(url_to_test)  # most fast
+    print get_domain_name_2(url_to_test)  # medium
