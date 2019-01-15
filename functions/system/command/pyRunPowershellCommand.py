fb82ad24925a192def9e4d93a7f34a2753607605
DingGuodong <uberurey_ups@163.com>
2019-01-15 14:23:18 +0800

    run Powershell command in Python on Windows

---
+++ functions/system/command/pyRunPowershellCommand.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:pyRunPowershellCommand.py
+Version:                0.0.1
+Author:                 Guodong
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2019/1/15
+Create Time:            13:10
+Description:            run Powershell command in Python on Windows
+Long Description:       
+References:             
+Prerequisites:          []
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+
+import subprocess
+
+ps_command = '(Get-Process lsass).Responding'  # get process responding status on Windows
+
+command = "powershell.exe " + ps_command
+p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+(stdout, stderr) = p.communicate()
+
+print stdout.strip()
