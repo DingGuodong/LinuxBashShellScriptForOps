556e55fcd59c879cdea76b21e2bc205c422aa73e
DingGuodong <uberurey_ups@163.com>
2019-03-20 14:57:36 +0800

    update: ask user yes or no to confirm or continue in bash

--- functions/options/askUserYesNoToConfirmOrContinue.sh
+++ functions/options/askUserYesNoToConfirmOrContinue.sh
@@ -8,15 +8,21 @@
 # Note:                 
 # Prerequisite:         
 # Description:          ask user yes or no to confirm or continue in bash
-# Reference:            
+# Reference:            /usr/share/yum-cli/cli.py
+#                       /usr/share/yum-cli/output.py
+#                       /usr/lib/python2.7/site-packages/yum/plugins.py
 
-read -n1 -r -p "Enter the y or Y to continue:" user_answer  # read -n1 -r -p "Press any key to continue..." key
-if [ "${user_answer}" != "y" ] && [ "${user_answer}" != "Y" ]; then
-    echo
-    exit 1
-else
-    echo
-fi
+function confirm_continue(){
+    echo "Is this ok? "
+    read -n 1 -r -p "Enter the y or Y to continue:" user_answer  # read -n1 -r -p "Press any key to continue..." key
+    if [[ "${user_answer}" != "y" ]] && [[ "${user_answer}" != "Y" ]]; then
+        echo -e "\n\nExiting on user cancel."  # exiting because "Download Only" specified
+        exit 1
+    else
+        echo
+    fi
+}
 
+confirm_continue
 # main job
 echo "foo"
