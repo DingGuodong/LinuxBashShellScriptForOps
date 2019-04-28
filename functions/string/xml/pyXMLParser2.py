8ae1d509cc0dd75e3bdd6b37f4186f3d8ff88370
DingGuodong <uberurey_ups@163.com>
2019-04-28 15:34:33 +0800

    find tag or text in XML file with built modules

--- functions/string/xml/pyXMLParser2.py
+++ functions/string/xml/pyXMLParser2.py
@@ -11,8 +11,8 @@ URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOp
 Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
 Create Date:            2018/7/27
 Create Time:            9:25
-Description:            an example about find tag or text in XML file
-Long Description:       
+Description:            an example of find tag or text in XML file with built modules
+Long Description:
 References:             
 Prerequisites:          []
 Development Status:     3 - Alpha, 5 - Production/Stable
@@ -31,38 +31,18 @@ try:
     import xml.etree.cElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
-from lxml import etree
-import os
-import re
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
 
 test_xml_file_url = 'https://raw.githubusercontent.com/apache/tomcat/trunk/conf/server.xml'
-xml_filename = 'xml_file.txt'
+xml_string = requests.get(test_xml_file_url).content
+xml_file = StringIO.StringIO(xml_string)
 
+new_tree = ET.ElementTree(file=xml_file)
 
-def get_xml_file():
-    with open(xml_filename, 'wb') as f:
-        f.write(requests.get(test_xml_file_url).content)
-
-
-if not os.path.exists(xml_filename):
-    get_xml_file()
-
-with open(xml_filename, 'r') as f:
-    xml_string = f.read()
-
-tree = etree.fromstring(xml_string)
-etree.strip_tags(tree, etree.Comment)
-xml_string_without_comment = etree.tostring(tree)
-
-xml_string_without_comment_blank = re.sub('\s*\n', '\n', xml_string_without_comment)  # remove blank lines
-# wu1 = os.linesep.join([line for line in xml_string_without_comment.splitlines() if line.strip()])  # line separators
-# wu2 = "".join([line for line in xml_string_without_comment.splitlines(True) if line.strip()])
-# wu3 = "".join(filter(str.strip, xml_string_without_comment.splitlines(True)))
-
-with open(xml_filename, 'w') as f:
-    f.write(xml_string_without_comment_blank)
-
-new_tree = ET.ElementTree(file=xml_filename)
-
-for element in new_tree.findall('Service/Connector'):
-    print(element.attrib)
+for element in new_tree.findall('Service/Connector'):  # <type 'Element'>
+    # print(element.attrib)
+    if element.get('protocol') == 'HTTP/1.1':
+        print element.get('port')
