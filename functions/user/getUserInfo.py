575585731fca8bf5a3652d8d35d344230ffa7a15
DingGuodong <uberurey_ups@163.com>
2019-06-20 10:27:04 +0800

    add more code snippets

--- functions/user/getUserInfo.py
+++ functions/user/getUserInfo.py
@@ -13,15 +13,19 @@ import sys
 
 mswindows = (sys.platform == "win32")
 linux2 = (sys.platform == "linux2")
+
 if mswindows:
     platform = "Windows"
     user = os.getenv('USERNAME')
 elif linux2:
     platform = "Linux"
-    user = os.getenv('USER') or os.getenv('LOGNAME')
+    user = os.getenv('USER') or os.getenv('LOGNAME')  # set|&grep =root
 else:
+    platform = ""
     user = None
-user_home = os.path.expanduser('~')  # both Windows and Linux is works
-print "current platform: %s" % platform
-print "current user: %s" % user
-print "current user home: %s" % user_home
+
+user_home = os.path.expanduser('~')  # both Windows and Linux is works, 颚化符 (~)
+
+print("current platform: %s" % platform)
+print("current user: %s" % user)
+print("current user home: %s" % user_home)
