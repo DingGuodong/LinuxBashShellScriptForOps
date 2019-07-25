4a073c625f7ddee264bc0dd983bfce4c5a5ef3fc
DingGuodong <uberurey_ups@163.com>
2019-07-25 10:18:42 +0800

    make code easy to use, thanks @jokimina

--- projects/LinuxSystemOps/Services/watch-dog-for-service.sh
+++ projects/LinuxSystemOps/Services/watch-dog-for-service.sh
@@ -11,7 +11,7 @@
 # Description:            a watch dog for a service written in bash shell script
 # Long Description:       check service running status, bring it up when service fail upto n times
 # Usage:                  */1 * * * * /root/watch-dog-for-service.sh
-# References:             
+# References:             Tips: 写代码尽量写得优雅，但优雅的前提是让他人更容易读懂和维护。
 # Prerequisites:          []
 # Development Status:     3 - Alpha, 5 - Production/Stable
 # Environment:            Console
@@ -29,6 +29,13 @@ MAX_TRY_TIMES=1 # 1 means restart service after 2 times failure
 ENABLE_DEBUG=1
 
 
+function log_message(){
+    if [[ -n "${1:-}" ]]; then
+        [[ "$ENABLE_DEBUG" == 1 ]] && echo "$(date +%Y%m%d%H%M%S,%s) $@" | tee -a ${WDFS_LOG_FILE}
+    fi
+
+}
+
 
 function check_service_status(){
     status="0"
@@ -41,36 +48,41 @@ function check_service_status(){
 
 }
 
+
 function get_db_value(){
     value_in_db=`cat ${WDFS_DB_FILE}`
     echo ${value_in_db}  # `echo` is more better than `cat` because of it is more easy to understand
 }
+
 
 function db_value_increase_one(){
     value_in_db=`get_db_value`
     echo "$(expr ${value_in_db} + 1)" > ${WDFS_DB_FILE}
 }
 
+
 function set_db_value_to_zero(){
     echo 0 > ${WDFS_DB_FILE}
 }
+
 
 function bring_up_service(){
     sudo -u ebt -i bash -c "cd /opt/corp_name/apps/app_name && /usr/bin/nohup /opt/tools/node-v6.9.5-linux-x64/bin/npm run start --app.name app_name >> /opt/corp_name/logs/app_name/app.log 2>&1 &"
 
 }
 
+
 function watch_dog(){
     status="0"
     check_service_status || status="$?"
     if [[ "$status" = 0 ]]; then
         set_db_value_to_zero
-        [[ "$ENABLE_DEBUG" == 1 ]] && echo "$(date +%Y%m%d%H%M%S,%s) ok: process is running" | tee -a ${WDFS_LOG_FILE}
+        log_message "ok: process is running"
         return 0
     else
         value_in_db=`get_db_value`
         if [[ "$value_in_db" == "$MAX_TRY_TIMES" ]]; then
-            [[ "$ENABLE_DEBUG" == 1 ]] && echo "$(date +%Y%m%d%H%M%S,%s) err: process is not found upto max times, try to start it" | tee -a ${WDFS_LOG_FILE}
+            log_message "err: process is not found upto max times, try to start it"
             bring_up_service
             return 0
         else
@@ -78,7 +90,7 @@ function watch_dog(){
         fi
 
         value_in_db=`get_db_value`
-        [[ "$ENABLE_DEBUG" == 1 ]] && echo "$(date +%Y%m%d%H%M%S,%s) warn: process is not found, current times is $value_in_db" | tee -a ${WDFS_LOG_FILE}
+        log_message "warn: process is not found, current times is $value_in_db"
 
     fi
 
