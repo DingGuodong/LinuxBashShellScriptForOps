567b56e2a177136ff5eb747ecd75bf7ccc9bf4e0
DingGuodong <uberurey_ups@163.com>
2019-08-29 10:24:38 +0800

    Python job scheduling for humans

---
+++ functions/others/scheduler/py-schedule-examples.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:py-schedule-examples.py
+Version:                0.0.1
+Author:                 dgden
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2019/8/29
+Create Time:            9:29
+Description:            Python job scheduling for humans
+Long Description:       
+References:             https://github.com/dbader/schedule
+                        https://schedule.readthedocs.io/en/stable/
+Prerequisites:          pip2.7 install schedule
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+
+import time
+
+import schedule
+
+
+def job():
+    print("I'm working...")
+
+
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+schedule.every(5).to(10).minutes.do(job)
+schedule.every().monday.do(job)
+schedule.every().wednesday.at("13:15").do(job)
+schedule.every().minute.at(":17").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
