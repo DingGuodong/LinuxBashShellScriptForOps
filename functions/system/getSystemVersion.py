13a0d2286e254502d65f345fdf3ce0d344339ecb
DingGuodong <uberurey_ups@163.com>
2019-09-16 14:25:54 +0800

    quality update

--- functions/system/getSystemVersion.py
+++ functions/system/getSystemVersion.py
@@ -12,6 +12,7 @@ import codecs
 import locale
 import os
 import platform
+import socket
 import subprocess
 import sys
 import time
@@ -37,16 +38,11 @@ DEFAULT_LOCALE_ENCODING = get_system_encoding()
 mswindows = (sys.platform == "win32")  # learning from 'subprocess' module
 linux = (sys.platform == "linux2")
 
-hidden_hostname = False
+print(platform.node() or socket.gethostname())
+print(platform.uname())
 
 if mswindows:
-    uname = list(platform.uname())
-    if hidden_hostname:
-        uname[1] = "hidden_hostname"
-    print tuple(uname)
-
     import _winreg
-
     try:
         reg_key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion")
         if reg_key:
@@ -61,8 +57,11 @@ if mswindows:
                 CurrentVersion = _winreg.QueryValueEx(reg_key, "CurrentVersion")[0] or None
                 BuildLabEx = _winreg.QueryValueEx(reg_key, "BuildLabEx")[0][:9] or None
                 ProductId = _winreg.QueryValueEx(reg_key, "ProductId")[0] or None
-                print (
-                    InstallDate, ProductName, EditionId, ReleaseId, CurrentVersion, CurrentBuild, BuildLabEx, ProductId)
+                UBR = _winreg.QueryValueEx(reg_key, "UBR")[0] or None
+                print(
+                    InstallDate, ProductName, EditionId, ReleaseId, CurrentVersion, CurrentBuild, BuildLabEx, ProductId,
+                    UBR)
+                print(ProductName, ReleaseId, InstallDate, CurrentBuild, UBR)
             elif InstallationType and InstallationType == 'Server':
                 InstallDate = time.strftime('%Y-%m-%d %H:%M:%S',
                                             time.localtime(_winreg.QueryValueEx(reg_key, "InstallDate")[0]))
@@ -72,11 +71,16 @@ if mswindows:
                 CurrentBuild = _winreg.QueryValueEx(reg_key, "CurrentBuild")[0] or None
                 BuildLabEx = _winreg.QueryValueEx(reg_key, "BuildLabEx")[0][:9] or None
                 if CurrentVersion <= '6.1':  # 6.1 include 'Windows Server 2008 R2(6.1)'
-                    print (InstallDate, ProductName, EditionId, CurrentVersion, CurrentBuild, BuildLabEx)
+                    print(InstallDate, ProductName, EditionId, CurrentVersion, CurrentBuild, BuildLabEx)
                 else:
                     ProductId = _winreg.QueryValueEx(reg_key, "ProductId")[
                                     0] or None  # Windows Server 2008 R2 is not supported
-                    print (InstallDate, ProductName, EditionId, CurrentVersion, CurrentBuild, BuildLabEx, ProductId)
+                    print(InstallDate, ProductName, EditionId, CurrentVersion, CurrentBuild, BuildLabEx, ProductId)
+        reg_key_machine_id = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\SQMClient")
+        if reg_key_machine_id:
+            MachineId = _winreg.QueryValueEx(reg_key_machine_id, "MachineId")[0] or None
+            print(MachineId)
+
     except WindowsError as e:
         print 'WindowsError is captured.'
         print e
@@ -88,12 +92,7 @@ if mswindows:
         print e.message.decode(DEFAULT_LOCALE_ENCODING)
 
 if linux:
-    uname = list(platform.uname())
-    if hidden_hostname:
-        uname[1] = "hidden_hostname"
-    print uname
-
-    proc_obj = subprocess.Popen(r'uname -a', shell=True, stdout=subprocess.PIPE,
+    proc_obj = subprocess.Popen(r'lsb_release -idrc', shell=True, stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT)
     result = proc_obj.stdout.read().strip().decode(DEFAULT_LOCALE_ENCODING)
     if result:
