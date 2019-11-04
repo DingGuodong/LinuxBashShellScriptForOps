9120734369b42a2e1e3fbb8bfdc131332f61a834
DingGuodong <uberurey_ups@163.com>
2019-11-04 16:12:04 +0800

    bug fix and update

--- functions/net/tcp/ssh/pySshConnectOps.py
+++ functions/net/tcp/ssh/pySshConnectOps.py
@@ -12,12 +12,19 @@ import paramiko
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect("120.27.194.133", port=22, username="root",
-               key_filename="C:\Users\Guodong\.ssh\ebt-linux-centos-ssh-root-key.pem", timeout=5)
+client.connect("192.168.88.19", port=22, username="root",
+               key_filename=r"C:\Users\dgden\.ssh\exportedkey201310171355", timeout=5)
 
-# TODO(Guodong Ding) paramiko add customized environment issue, next line not works on centos and ubuntu
-stdin, stdout, stderr = client.exec_command('echo "x$user_defined_variable_name"',
+
+stdin, stdout, stderr = client.exec_command('echo "x$user_defined_variable_name x"',
                                             environment={"user_defined_variable_name": "1"}, get_pty=True)
+"""
+warning::   
+            The server may reject this request depending on its ``AcceptEnv``
+            setting; such rejections will fail silently (which is common client
+            practice for this particular request type). Make sure you
+            understand your server's configuration before using!
+"""
 for line in stdout:
     print line,
 client.close()
