ad78bda5aa9fbb317e5fe3338519fb34ece41edf
DingGuodong <uberurey_ups@163.com>
2020-02-24 10:38:09 +0800

    python library for unrar/unzip/untar a file

---
+++ functions/file/compress_and_extract/unrar-a-file.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:unrar-a-file.py
+Version:                0.0.1
+Author:                 dgden
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2020/2/22
+Create Time:            16:10
+Description:            python library for unrar/unzip/untar a file
+Long Description:       
+References:             https://stackoverflow.com/questions/17614467/how-can-unrar-a-file-with-python
+                        https://stackoverflow.com/questions/31261879/how-to-create-a-rar-file-using-python
+                        https://rarfile.readthedocs.io/en/latest/
+                        http://pyunpack.readthedocs.org/
+                        http://pypi.python.org/pypi/patool
+
+Prerequisites:          pip install pyunpacsubprocessk
+                        pip install rarfile  # import rarfile
+                        pip install patool  # import patoolib
+
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+
