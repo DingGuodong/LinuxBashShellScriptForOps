b6dc6ded9aaf7d0bc88baa77f234bf3824b9f0f0
DingGuodong <uberurey_ups@163.com>
2020-09-01 16:23:05 +0800

    add more csv examples

---
+++ functions/file/csv/python-csv-reader.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf-8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:python-csv-reader.py
+Version:                0.0.1
+Author:                 Guodong
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2018/7/31
+Create Time:            10:21
+Description:            
+Long Description:       
+References:             
+Prerequisites:          []
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 3
+Topic:                  Utilities
+ """
+import csv
+
+csv_file = 'simple.csv'
+
+with open(csv_file, 'r', encoding="utf-8") as f:
+    csv_reader = csv.reader(f)  # a generator object
+    for row in csv_reader:
+        print(row)
+
+with open(csv_file, 'r', encoding="utf-8") as f:
+    csv_DictReader = csv.DictReader(f)
+    for row in csv_DictReader:
+        print(row.get('Name'))
--- functions/file/csv/python-csv-writer.py
+++ functions/file/csv/python-csv-writer.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
-# encoding: utf-8
 # -*- coding: utf-8 -*-
 """
 Created by PyCharm.
-File Name:              LinuxBashShellScriptForOps:pyCSVReader.py
+File Name:              LinuxBashShellScriptForOps:python-csv-writer.py
 Version:                0.0.1
-Author:                 Guodong
+Author:                 dgden
 Author Email:           dgdenterprise@gmail.com
 URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
 Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
-Create Date:            2018/7/31
-Create Time:            10:21
+Create Date:            2020/9/1
+Create Time:            14:06
 Description:            
 Long Description:       
 References:             
@@ -21,19 +20,18 @@ Intended Audience:      System Administrators, Developers, End Users/Desktop
 License:                Freeware, Freely Distributable
 Natural Language:       English, Chinese (Simplified)
 Operating System:       POSIX :: Linux, Microsoft :: Windows
-Programming Language:   Python :: 3
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
 Topic:                  Utilities
  """
 import csv
 
-csv_file = 'simple.csv'
+lines = """Email                  ,Name         ,NewEmail                   ,Status
+chris1.ding@example.com,Chris.Ding丁国栋,chris1.ding@new_example.com,0
+chris2.ding@example.com,Chris.Ding丁国栋,chris2.ding@new_example.com,0
+"""
 
-with open(csv_file, 'r') as f:
-    csv_reader = csv.reader(f)  # a generator object
-    for row in csv_reader:
-        print(row)
-
-with open(csv_file, 'r') as f:
-    csv_DictReader = csv.DictReader(f)
-    for row in csv_DictReader:
-        print(row.get('Name'))
+# note: newline may not work well on Windows
+with open("simple.csv", 'w', encoding="utf-8", newline='\n') as fp:
+    writer = csv.writer(fp)
+    writer.writerows([line.split(",") for line in lines.strip().split("\n")])
