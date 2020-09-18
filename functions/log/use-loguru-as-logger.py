c9b423a8272461f77c4c1f7cc903ae6050e91fdd
DingGuodong <uberurey_ups@163.com>
2020-09-18 11:56:17 +0800

    using loguru make logging simple and stupid

---
+++ functions/log/use-loguru-as-logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created by PyCharm.
+File Name:              LinuxBashShellScriptForOps:use-loguru-as-logger.py
+Version:                0.0.1
+Author:                 dgden
+Author Email:           dgdenterprise@gmail.com
+URL:                    https://github.com/DingGuodong/LinuxBashShellScriptForOps
+Download URL:           https://github.com/DingGuodong/LinuxBashShellScriptForOps/tarball/master
+Create Date:            2020/9/18
+Create Time:            11:37
+Description:            loguru is python3 module make logging simple
+Long Description:       
+References:             [loguru](https://github.com/Delgan/loguru)
+Prerequisites:          pip3.8 install loguru
+Development Status:     3 - Alpha, 5 - Production/Stable
+Environment:            Console
+Intended Audience:      System Administrators, Developers, End Users/Desktop
+License:                Freeware, Freely Distributable
+Natural Language:       English, Chinese (Simplified)
+Operating System:       POSIX :: Linux, Microsoft :: Windows
+Programming Language:   Python :: 2.6
+Programming Language:   Python :: 2.7
+Topic:                  Utilities
+ """
+print("Deprecated: try to use it in Python3.x")
