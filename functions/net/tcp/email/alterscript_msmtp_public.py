0278e1c9e89f3ff98ca45594d649d93fe0b3ef08
DingGuodong <uberurey_ups@163.com>
2021-05-06 20:31:06 +0800

    bug fix and support ssl, tls, no encryption

--- functions/net/tcp/email/alterscript_msmtp_public.py
+++ functions/net/tcp/email/alterscript_msmtp_public.py
@@ -14,6 +14,8 @@ import smtplib
 import string
 import sys
 
+import six
+
 
 def usage():
     print("""
@@ -23,16 +25,24 @@ def usage():
                     https://hostname/zabbix.php?action=mediatype.edit&mediatypeid=4
                     Script parameters: {ALERT.SENDTO} {ALERT.SUBJECT} {ALERT.MESSAGE}
     Example: python %s "admin@example.domain" "Test email from Python" "Python rules them all!"
-""") % (__file__, sys.argv[0])
+""" % (__file__, sys.argv[0]))
     sys.exit(0)
 
 
 EMAIL_HOST = "smtp.example.domain"  # change it
 EMAIL_PORT = 25  # default smtp port
+EMAIL_HOST_ENABLE_SSL = False  # the most frequently used option
+EMAIL_HOST_ENABLE_TLS = False  # not all smtp server support TLS
 EMAIL_HOST_USER = 'noreply@example.domain'  # change it
 EMAIL_HOST_PASSWORD = 'your password'  # change it
 DEFAULT_FROM_EMAIL = 'noreply@example.domain'  # change it, https://tools.ietf.org/html/rfc822.html#appendix-A
 CRLF = "\r\n"  # for Windows user read easily
+
+if EMAIL_HOST_ENABLE_SSL and EMAIL_HOST_ENABLE_TLS:
+    raise Exception("can NOT be used together")
+
+if EMAIL_HOST_ENABLE_SSL:
+    EMAIL_PORT = 465
 
 # user defined variable, in Zabbix is {ALERT.SENDTO}
 EMAIL_TO = "example@example.domain"
@@ -53,17 +63,37 @@ else:
         EMAIL_SUBJECT = sys.argv[2]
         EMAIL_BODY = sys.argv[3]
 
-BODY = string.join((
-    "From: %s" % DEFAULT_FROM_EMAIL,
-    "To: %s" % EMAIL_TO,
-    "Subject: %s" % EMAIL_SUBJECT,
-    "",
-    EMAIL_BODY
-), CRLF)
+if six.PY2:
+    BODY = string.join((
+        "From: %s" % DEFAULT_FROM_EMAIL,
+        "To: %s" % EMAIL_TO,
+        "Subject: %s" % EMAIL_SUBJECT,
+        "",
+        EMAIL_BODY
+    ), CRLF)
+else:
+    BODY = CRLF.join((
+        "From: %s" % DEFAULT_FROM_EMAIL,
+        "To: %s" % EMAIL_TO,
+        "Subject: %s" % EMAIL_SUBJECT,
+        "",
+        EMAIL_BODY
+    ))
 
-server = smtplib.SMTP()
+if EMAIL_HOST_ENABLE_SSL or EMAIL_HOST_ENABLE_TLS:
+    if six.PY3:
+        # for python3, ValueError: server_hostname cannot be an empty string or start with a leading dot.
+        server = smtplib.SMTP_SSL(host=EMAIL_HOST)
+    else:
+        server = smtplib.SMTP_SSL()
+else:
+    server = smtplib.SMTP()
+
 server.connect(EMAIL_HOST, EMAIL_PORT)
-server.starttls()
+
+if EMAIL_HOST_ENABLE_TLS:
+    server.starttls()
+
 server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
 server.sendmail(DEFAULT_FROM_EMAIL, [EMAIL_TO], BODY)
 server.quit()
