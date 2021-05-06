24cc340a96e25c8b92225c4e65154c3af54e57eb
DingGuodong <uberurey_ups@163.com>
2021-05-06 17:04:59 +0800

    remove warnings by shell check

--- functions/net/tcp/email/alertscripts_msmtp.sh
+++ functions/net/tcp/email/alertscripts_msmtp.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
-
-# Zabbix Email alter shell script
+# Zabbix Email alert shell script(**Deprecated**)
 # msmtp is an SMTP client
+# TODO(DingGuodong) set a configuration for msmtp
+# refer: https://askubuntu.com/questions/1289573/msmtp-sendmail-account-default-not-found-no-configuration-file-available-sys
+
 DEBUG=1
-if [[ ${DEBUG} -gt 0 ]];then
-    exec 2>>/tmp/zabbix_msmtp.log
-    set -x
+if [[ ${DEBUG} -gt 0 ]]; then
+  exec 2>>/tmp/zabbix_msmtp.log
+  set -x
 fi
 FROM='example@example.com'
 account_name='zabbix'
@@ -17,7 +19,7 @@ recipient=$1
 subject=$2
 message=$3
 date=$(date --rfc-2822)
-sed 's/$/\r/' <<eof | /usr/bin/msmtp --account ${account_name} ${recipient}
+sed 's/$/\r/' <<eof | /usr/bin/msmtp --account ${account_name} "${recipient}"
 From: <${FROM}>
 To: <${recipient}>
 Subject: ${subject}
