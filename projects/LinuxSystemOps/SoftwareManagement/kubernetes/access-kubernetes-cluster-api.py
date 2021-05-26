fbf440e4c5f06b9ec978667b9613c3927c8a4a67
DingGuodong <uberurey_ups@163.com>
2021-05-26 12:17:04 +0800

    add docs and references.

--- projects/LinuxSystemOps/SoftwareManagement/kubernetes/access-kubernetes-cluster-api.py
+++ projects/LinuxSystemOps/SoftwareManagement/kubernetes/access-kubernetes-cluster-api.py
@@ -13,7 +13,43 @@ Create Time:            19:57
 Description:            get kubernetes cluster info by using kubernetes SDK
 
 Long Description:
-References:             [使用 Kubernetes API 访问集群](https://kubernetes.io/zh/docs/tasks/administer-cluster/access-cluster-api/#python-client)
+References:             [使用 Kubernetes API 访问集群]
+                            (https://kubernetes.io/zh/docs/tasks/administer-cluster/access-cluster-api/#python-client)
+                        [使用 kubeconfig 文件组织集群访问]
+                            (https://kubernetes.io/zh/docs/concepts/configuration/organize-cluster-access-kubeconfig/)
+                            默认情况下，kubectl 在 $HOME/.kube 目录下查找名为 config 的文件。
+                        [从 Pod 中访问 Kubernetes API]
+                            (https://kubernetes.io/zh/docs/tasks/run-application/access-api-from-pod/)
+                        [Shows how to load a Kubernetes config from within a cluster]
+                            (https://github.com/kubernetes-client/python/blob/master/examples/in_cluster_config.py)
+                        [Shows how to load a Kubernetes config from outside of the cluster]
+                            (https://github.com/kubernetes-client/python/blob/master/examples/out_of_cluster_config.py)
+If you get 403 errors from the API server you will have to configure RBAC to
+add permission to list pods by applying the following manifest:
+## cc: a prefix which short for `customer customized`
+---
+kind: ClusterRole
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: cc-nodes-pods-read
+rules:
+- apiGroups: [""]
+  resources: ['nodes', "pods"]
+  verbs: ["get", "list"]
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1
+metadata:
+  name: cc-nodes-pods-read
+subjects:
+- kind: ServiceAccount
+  name: default
+  namespace: default
+roleRef:
+  kind: ClusterRole
+  name: cc-nodes-pods-read
+  apiGroup: rbac.authorization.k8s.io
+
 Prerequisites:          pip install kubernetes
 Development Status:     3 - Alpha, 5 - Production/Stable
 Environment:            Console
@@ -27,7 +63,8 @@ Topic:                  Utilities
  """
 from kubernetes import client, config
 
-config.load_kube_config("c:\\users\\z\\desktop\\config")
+# use `config.load_incluster_config()` when access kubernetes api from within a cluster
+config.load_kube_config("c:\\users\\z\\desktop\\config")  # load a Kubernetes config from outside of the cluster
 
 v1 = client.CoreV1Api()
 
